49ad46bdf3d5f0ec7b09ca50fc198fada3bb848a
mathias-primero <goncalves.almeida.matheus@escola.pr.gov.br>
2025-05-14 08:46:52 -0300

    Update perguntas.js
    dar cara para o projeto

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,24 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'conhecimentos gerais, historia do plno real.',
+    'Quantas transformações o ouveram antes de chegar no plano real?.',
+    'Overam 12 transformações antes de chegarmos no plano real.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'conhecimentos gerais, redes sociais',
+    'Qual o assunto mais comentado da atualidade?',
+    'o assunto mais comentado atualmente é sobre o novo papa, Leão 14.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Qimica',
+    'O que e uma ligaçõa metalica. Quais suas caracteristicas',
+    'Ligação metalica e uma ligação que ocorre em metais, caracterizada por conduzir elétricidade e calor, além de ter altos pontos
+    de ebolição e condenssação.'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como usar a regra do ly?',
+    'a palavra ly = mente das palavres em português, por exeplo lindo, lindamente.'
 )
